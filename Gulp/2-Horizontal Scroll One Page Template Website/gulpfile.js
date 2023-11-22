7ab98498df8740499c31f6408035955bbdaf1270
David Ramírez <rrdavid08@gmail.com>
2023-11-22 11:56:42 +0100

    Proyecto GULP Horizontal Scroll hecho

---
+++ Gulp/2-Horizontal Scroll One Page Template Website/gulpfile.js
@@ -0,0 +1,41 @@
+const { src, dest, parallel, series} = require('gulp');
+const compileSASS = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
+const cleanJS = require('gulp-uglify');
+const concat = require('gulp-concat');
+
+
+function compilarSASS() {
+   return src('src/scss/slides.scss')
+      .pipe(compileSASS())
+      .pipe(dest('src/css/'));
+}
+
+function limpiarCSS() {
+   return src('src/css/*.css')
+      .pipe(cleanCSS())
+      .pipe(dest('dist/css/'));
+}
+
+function limpiarJS() {
+   return src('src/js/*.js')
+      .pipe(cleanJS())
+      .pipe(dest('dist/js/'));
+}
+
+function concatenarCSS() {
+   return src('dist/css/*.css')
+      .pipe(concat('index.bundle.css'))
+      .pipe(dest('dist/css/'));
+}
+
+function concatenarJS() {
+   return src('dist/js/*.js')
+      .pipe(concat('index.bundle.js'))
+      .pipe(dest('dist/js/'));
+}
+
+exports.limpiarGeneral = parallel(limpiarCSS, limpiarJS);
+exports.concatenarGeneral = parallel(concatenarCSS, concatenarJS);
+
+exports.default = series(compilarSASS, exports.limpiarGeneral, exports.concatenarGeneral);
