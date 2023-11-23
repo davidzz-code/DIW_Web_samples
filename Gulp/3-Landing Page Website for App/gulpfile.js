58fc6d7cad3743cc63dc0f755455b984e081a70f
David Ramírez <rrdavid08@gmail.com>
2023-11-23 19:36:13 +0100

    Proyecto gulp 3 acabado

---
+++ Gulp/3-Landing Page Website for App/gulpfile.js
@@ -0,0 +1,48 @@
+const { series, parallel, src, dest } = require('gulp');
+const compileSASS = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
+const cleanJS = require('gulp-uglify');
+const concat = require('gulp-concat');
+
+/* Compilar SASS */
+
+function compilarSASS() {
+   return src('src/scss/slides.scss')
+      .pipe(compileSASS())
+      .pipe(dest('src/css/'));
+}
+
+
+/* Minimiar CSS y JS */
+
+function limpiarCSS() {
+   return src('src/css/*.css')
+   .pipe(cleanCSS())
+   .pipe(dest('dist/css/'));
+}
+
+function limpiarJS() {
+   return src('src/js/*.js')
+   .pipe(cleanJS())
+   .pipe(dest('dist/js/'));
+}
+
+
+/* Concatenar CSS y JS */
+function concatenarCSS() {
+   return src('dist/css/*.css')
+   .pipe(concat('index.bundle.css'))
+   .pipe(dest('dist/css'));
+}
+
+function concatenarJS() {
+   return src('dist/js/*.js')
+   .pipe(concat('index.bundle.js'))
+   .pipe(dest('dist/js'));
+}
+
+exports.limpiar = parallel(limpiarCSS, limpiarJS)
+exports.concatenar = parallel(concatenarJS, concatenarCSS)
+
+exports.default = series(compilarSASS, exports.limpiar, exports.concatenar);
+
